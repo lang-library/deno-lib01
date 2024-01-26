72bd4bb3f67971b070d6967762f0b9d01dfdd099
JavaCommons Technologies <javacommons@gmail.com>
2024-01-26 22:09:14 +0900

    .

--- add2.js
+++ add2.js
@@ -1,3 +1,3 @@
 export function add2(a, b) {
-  return a + b;
+  return (a + b) * 10;
 }
---
+++ remote.js
@@ -0,0 +1,3 @@
+import { add2 } from 'https://github.com/lang-library/deno-lib01/raw/main/add2.js'
+
+console.log(add2(11, 22));
