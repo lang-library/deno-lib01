585dbbb3e954bfeb8ac6531e63fb1109acdfdb41
JavaCommons Technologies <javacommons@gmail.com>
2024-01-26 22:15:32 +0900

    .

--- remote.js
+++ remote.js
@@ -1,3 +1,3 @@
-import { add2 } from 'https://github.com/lang-library/deno-lib01/raw/main/add2.js'
+import { add2 } from 'https://github.com/lang-library/deno-lib01/raw/2024.0126.2212/add2.js'
 
 console.log(add2(11, 22));
